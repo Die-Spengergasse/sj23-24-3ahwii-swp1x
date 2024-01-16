b991065d76f8a8f4a73e06319855117f52a6ae80
Georg Graf (SPG) <grafg@spengergasse.at>
2024-01-16 10:49:58 +0100

    sü + hü

--- 2024-01-09__bmi/app.js
+++ 2024-01-09__bmi/app.js
@@ -1,17 +1,31 @@
+// So liegt der Normalwert bei Männern laut Deutscher Gesellschaft für Ernährung
+// im Intervall von 20 bis 25 kg / m², während er sich
+// bei Frauen im Intervall von 19 bis 24 kg / m² befindet.
+
 class Person {
     /* Gewicht in kg, Größe in m */
-    name;
+    #name;
     #gewicht;
-    groesse;
-    constructor(name, gewichtPar, groesse) {
-        this.name = name;
+    #groesse;
+    constructor(namePar, gewichtPar, groessePar) {
+        this.name = namePar;
         this.gewicht = gewichtPar;
-        this.groesse = groesse;
+        this.groesse = groessePar;
     }
-    get bmi() {}
+
+    set name(namePar) {
+        if (typeof namePar !== 'string') {
+            throw new Error('ungültiger Name');
+        }
+        if (namePar.length < 3) {
+            throw new Error('Name zu kurz');
+        }
+        this.#name = namePar;
+    }
+
     set gewicht(gewichtPar) {
         // gewicht in kg
-        if (gewichtPar < 1 || gewichtPar > 500) {
+        if (gewichtPar < 10 || gewichtPar > 500) {
             throw new Error('ungültiges Gewicht');
         }
         this.#gewicht = gewichtPar;
@@ -19,17 +33,43 @@ class Person {
     get gewicht() {
         return this.#gewicht;
     }
+    set groesse(groessePar) {
+        if (groessePar < 0.5 || groessePar > 3.0) {
+            throw new Error('ungültige Groesse');
+        }
+        this.#groesse = groessePar;
+    }
+
+    get bmi() {
+        const nmbr = this.#gewicht / (this.#groesse * this.#groesse);
+        return Math.round(nmbr * 10) / 10;
+    }
     toString() {
-        return `${this.name} wiegt ${this.gewicht} kg und ist ${this.groesse} m groß`;
+        return (
+            'Name: ' +
+            this.#name +
+            ' Gewicht: ' +
+            this.#gewicht +
+            ' Größe: ' +
+            this.#groesse +
+            ' BMI: ' +
+            this.bmi
+        );
     }
 }
-p = new Person('Hans', 80, 1.8);
-console.log(p.gewicht);
-
 a = [
-    ['Tim', 70, 180],
-    ['Branka', 50, 160],
+    ['Peta', 90, 1.7, 'w'],
+    ['Lisa', 50, 3.5, 'w'], //Wie schaffe ich es, dass hier ein Fehler geworfen wird?
+    ['Roland', 70, 1.7, 'w'],
+    ['Hans', 80, 1.8, 'w'],
 ];
 
-b = a.map((arr) => new Person(...arr));
-b.forEach((p) => console.log(p.toString()));
+b = a.map((arr) => {
+    try {
+        return new Person(...arr);
+    } catch (e) {
+        console.log(e.message);
+        return null;
+    }
+}); // jetzt ist b ein Personen-Array
+b.forEach((p) => console.log(p + ''));
